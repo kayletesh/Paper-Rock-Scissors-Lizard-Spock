5734ceae30fd4036ac360b6d414b5e5a6ce96868
kayletesh <kayletesh@gmail.com>
2024-02-17 15:32:19 -0500

    Add Pseudocode set computer choice set player choice

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-alert("Hello World!");
+// get the computer's choice.
+function getComputerSelection() {
+    const selection [paper, rock, scissors, lizard, Spock];
+    const randomselection = math.floor(Math.random() *5);
+    return selection[randomSelection];
+}
+// create an array for the choices.
+// set randomizer for the choices.
+// return the choice.
+
+// get player choice
+// create a prompt to grab the player's choice and begin the game.
+// set player's choice to lowercase.
+//return choice.
+
+// create the function that will run the game, and house all results.
+// function should return the result of each matchup.
+
+//set the loop for 10 rounds.
+//this function will also hold the method to modify the score each round.
+//define values to set player score and computer score to 0 at the start of game.
+//create a for loop.
+// define current round, up to-round, and increment value in the for loop.
+//define computer choice and the player choice(set this one's () to current round)
+// define result as the function that plays the game.
+// console.log the result, as well as a message to show each round.
+// create an if statement inside the for loop to collect score data. set result.includes to gather the data desired. method in this function should be the incrementation of the score.
+// console.log current comp and player scores.
+// alert the results.
+//END FOR LOOP CODE
+
+//create a function to trigger after final round, called Game Over.
+//set parameters to final player score, and final comp score.
+//function should be an if statement.
+//set final win/lose condition.
+//alert final result.
+//dont forget to add how to play again.
