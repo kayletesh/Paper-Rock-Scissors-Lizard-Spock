d95b984463bb7c28251d6fce36ec188af139d493
kayletesh <kayletesh@gmail.com>
2024-02-19 10:07:46 -0500

    clean up psuedocode check for any remaining errors

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
 // get the computer's choice.
 function getComputerSelection() {
-  // create an array for the choices.
   const selection = ["paper", "rock", "scissors", "lizard", "spock"];
-  // set randomizer for the choices.
-  const randomSelection = Math.floor(Math.random() * 5);
-  // return the choice.
+  const randomSelection = Math.floor(Math.random() * selection.length);
   return selection[randomSelection];
 }
 
 // get player choice
 function getPlayerSelection(currentRound) {
-  // define player's selection
-  let playerSelection;
-  // create a prompt to grab the player's choice and begin the game.
-  playerSelection = prompt(
+  const playerSelection = prompt(
     `Round ${currentRound} FIGHT! Pick one: Paper, Rock, Scissors, Lizard, or Spock!`
-  );
-  // set player's choice to lowercase.
-  playerSelection = playerSelection.toLowerCase();
+  ).toLowerCase();
 
-  //return choice.
   return playerSelection;
 }
-
-// create the function that will run the game, and house all results.
 
 function playGame(playerSelection, computerSelection) {
   // tie statement
@@ -87,52 +76,49 @@ function playGame(playerSelection, computerSelection) {
   }
 }
 
-// function should return the result of each matchup.
-
-//create a function to trigger after final round, called Game Over.
 function gameOver(finalPlayerScore, finalComputerScore) {
-  if (playerScore > computerScore) {
+  if (finalPlayerScore > finalComputerScore) {
     return "YOU WIN!!!!";
-  } else if (playerScore < computerScore) {
+  } else if (finalPlayerScore < finalComputerScore) {
     return "YOU LOSE...";
   }
 }
 
-//set the loop for 10 rounds.
-//this function will also hold the method to modify the score each round.
-//define values to set player score and computer score to 0 at the start of game.
 let playerScore = 0;
 let computerScore = 0;
-//create a for loop.
-// define current round, up to-round, and increment value in the for loop.
-for (currentRound = 1; currentRound <= 10; currentRound++) {
-  //define computer choice and the player choice(set this one's () to current round)
-  const playerSelection = getPlayerSelection(currentRound);
-  const computerSelection = getComputerSelection();
-  // define result as the function that plays the game.
-  const result = playGame(playerSelection, computerSelection);
-  // console.log the result, as well as a message to show each round.
-  //   console.log(result);
-  // create an if statement inside the for loop to collect score data. set result.includes to gather the data desired. method in this function should be the incrementation of the score.
+const updateScore = (result) => {
   if (result.includes("WIN")) {
     playerScore++;
     console.log(`Round ${currentRound}... ${result}`);
+    return;
   } else if (result.includes("Lose")) {
     computerScore++;
     console.log(`Round ${currentRound}... ${result}`);
+    return;
   } else if (result.includes("ERROR")) {
-    currentRound--;
     console.log(
       `ERROR!! Resetting Round... Please Choose Either Paper, Rock, Scissors, Lizard, or Spock!`
     );
+    return "error";
   } else if (result.includes("Borg")) {
+    return "Borg";
+  }
+};
+
+for (currentRound = 1; currentRound <= 10; currentRound++) {
+  const playerSelection = getPlayerSelection(currentRound);
+  const computerSelection = getComputerSelection();
+  const result = playGame(playerSelection, computerSelection);
+  const updatedScore = updateScore(result);
+  if (updatedScore === "error") {
+    currentRound--;
+  } else if (updatedScore === "Borg") {
     currentRound = 99;
   }
-  // console.log current comp and player scores.
+
   console.log(
     `Computer Score = ${computerScore}, Player Score = ${playerScore}`
   );
-  // alert the results.
 
   if (currentRound === 99) {
     alert(
@@ -149,9 +135,3 @@ for (currentRound = 1; currentRound <= 10; currentRound++) {
     alert(result);
   }
 }
-
-//set parameters to final player score, and final comp score.
-//function should be an if statement.
-//set final win/lose condition.
-//alert final result.
-//dont forget to add how to play again.
